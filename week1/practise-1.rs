1681dd2fb5f948e0fa12ef9aff09750add4ecca9
iambob123 <ofor177649@gmail.com>
2023-10-19 18:01:38 +0200

    (my first commit)

---
+++ week1/practise-1.rs
@@ -0,0 +1,13 @@
+fn main(){
+	println!("Welcome to C0S101!")
+
+	println!("The course learning outcome is as follows:");
+
+	println!("1. Distinguish between computer and computer programming,/n
+		      2. Develop some techniques in computer science,/n
+		      3. Understand the different area of study in computer science
+		      4. Conversant with applications of computer science,/n
+		      5. Navigate the career prospects in computer science./n
+		      6. Conversant with computer programming concepts.");
+
+
