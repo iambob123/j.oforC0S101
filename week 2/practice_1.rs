21ea79fa546ff092ede3cad0146b195fc5311fb6
iambob123 <ofor177649@gmail.com>
2023-10-21 19:25:08 +0200

    week 2 commit

---
+++ week 2/practice_1.rs
@@ -0,0 +1,6 @@
+fn main(){
+	println!("Welcome to Rust Programming");
+
+	println!("Henceforth you shall be called RUSTACIANS!");
+
+}
---
+++ week 2/project_1.rs
@@ -0,0 +1,13 @@
+fn main () {
+	let p:f64 = 520_000_000.00;
+	let r:f64 = 10.0;
+	let n:f64 = 5.0;
+
+	//compound interest
+	let a = p * ( 1.0 +( r / 100.0 ))* n;
+	println!("Amount is {}", a);
+	let cl = a - p;
+	println!("Compound Interest is {}", cl);
+
+
+}
